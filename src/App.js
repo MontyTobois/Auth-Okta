342c4c124eacdf7088de1771e5d227c19d06377b
Andre-Tobois <tobois.andre@gmail.com>
2019-01-08 23:56:33 -0500

    feat: Used the router feature on app.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from  '@okta/okta-react'
 
 import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <Router>
+        <div className="App">
+       <Navbar />
+      <div className="container">
+        <Route path="/" exact={true} component={Home} />
+         <Route path="/staff" exact={true} component={Staff} />
       </div>
+
+      </div>
+      </Router>
     );
   }
 }
