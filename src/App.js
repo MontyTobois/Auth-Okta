3ac6d3f4ce277f3085e66fdae9104a27dca8bdd9
Andre-Tobois <tobois.andre@gmail.com>
2019-01-09 21:17:49 -0500

    feat: Setup the callbacks for app will handle actions in Login file.

--- src/App.js
+++ src/App.js
@@ -7,20 +7,31 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
 
+
+function onAuthRequired({history}) {
+  history.push('/login');
+}
 
 class App extends Component {
   render() {
     return (
       <Router>
+        <Security issuer='https://dev-145413.oktapreview.com/oauth2/default'
+                  client_id='0oaiun6b76jfkQ7bO0h7'
+                  redirect_uri={window.location.origin + '/implicit/callback'}
+                  onAuthRequired={onAuthRequired}>
         <div className="App">
-       <Navbar />
-      <div className="container">
-        <Route path="/" exact={true} component={Home} />
-         <Route path="/staff" exact={true} component={Staff} />
-      </div>
-
-      </div>
+          <Navbar />
+          <div className="container">
+            <Route path="/" exact={true} component={Home} />
+            <SecureRoute path="/staff" exact={true} component={Staff} />
+            <Route path='/login' render={() => <Login baseUrl='https://dev-145413.oktapreview.com' />} />
+          <Route path='/implicit/callback' component={ImplicitCallback} />
+          </div>
+        </div>
+        </Security>
       </Router>
     );
   }
